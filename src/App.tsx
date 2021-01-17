aa77d927066f04c34640c04c1097733368415f44
Oleksandr Shevchuk <s471652@gmail.com>
2021-01-17 12:14:22 +0200

    Structure for the client with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import NavBar from "./components/common/NavBar";
+import Users from "./components/Users";
+import NotFound from "./components/NotFound";
+// import LoginForm from "./components/LoginForm";
+// import Logout from "./components/Logout";
+// import RegisterForm from "./components/RegisterForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ToastContainer />
+      <NavBar user={{ name: "Shol" }} />
+      <main className="container">
+        <Switch>
+          {/* <Route path="/register" component={RegisterForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/logout" component={Logout} /> */}
+          <Route path="/users" component={Users} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/home" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
