52a380f737a1981eb55b77375c35be31a3d55910
Oleksandr Shevchuk <s471652@gmail.com>
2021-02-06 18:18:58 +0200

    Auth service -> TS

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,25 +2,28 @@ import config from "../config.json";
 const accessTokenKey = config.accessTokenKey;
 const refreshTokenKey = config.refreshTokenKey;
 
-export const loginWithJwt = (accessToken, refreshToken) => {
+export const loginWithJwt = (
+  accessToken: string,
+  refreshToken: string
+): void => {
   localStorage.setItem(accessTokenKey, accessToken);
   localStorage.setItem(refreshTokenKey, refreshToken);
 };
 
-export const logout = () => {
+export const logout = (): void => {
   localStorage.removeItem(accessTokenKey);
   localStorage.removeItem(refreshTokenKey);
 };
 
-export const getAccessToken = () => {
+export const getAccessToken = (): string | null => {
   return localStorage.getItem(accessTokenKey);
 };
 
-export const getRefreshToken = () => {
+export const getRefreshToken = (): string | null => {
   return localStorage.getItem(refreshTokenKey);
 };
 
-export const checkAuth = () => {
+export const checkAuth = (): boolean => {
   return (
     !!localStorage.getItem(accessTokenKey) &&
     !!localStorage.getItem(refreshTokenKey)
